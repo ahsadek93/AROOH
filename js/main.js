14c635096f0ee5cf83496000ce2499b7fd2ea502
ahsadek93 <62098165+ahsadek93@users.noreply.github.com>
2020-07-05 11:06:01 +0200

    3 pages up

---
+++ js/main.js
@@ -0,0 +1,20 @@
+    $(document).ready(function () {
+            $('.logSlider').slick({
+                infinite: true,
+                autoplay: 5000,
+                dots: false,
+                arrows: false,
+                fade:true
+            });
+        });
+    $(document).ready(function () {
+            $('.signUpSlider').slick({
+                infinite: true,
+                autoplay: 5000,
+                dots: false,
+                arrows: false,
+                fade:true
+            });
+        });
+
+        
