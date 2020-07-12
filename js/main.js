4ce839b5a4fec754f3d28f960da451367dde1c47
ahsadek93 <62098165+ahsadek93@users.noreply.github.com>
2020-07-12 10:33:25 +0200

    slider animation on sign in page

--- js/main.js
+++ js/main.js
@@ -18,4 +18,36 @@
         });
 
 
+
+
+        $('.ml14 .letters').html(function (index, html) {
+            return html.replace(/\S/g, '<span class="letter">$&</span>');
+        });
+
+
+        anime.timeline({loop: true})
+            .add({
+                targets: '.ml14 .line',
+                scaleX: [0, 1],
+                opacity: [0.5, 1],
+                easing: "easeInOutExpo",
+                duration: 900
+            }).add({
+                targets: '.ml14 .letter',
+                opacity: [0, 1],
+                translateX: [40, 0],
+                translateZ: 0,
+                scaleX: [0.3, 1],
+                easing: "easeOutExpo",
+                duration: 2100,
+                offset: '-=600',
+                // delay: (el, i) => 150 + 25 * i
+            }).add({
+                targets: '.ml14',
+                opacity: 0,
+                duration: 1000,
+                easing: "easeOutExpo",
+                delay: 30
+            });
+
  
